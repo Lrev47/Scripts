48be9884f9decc72bcd38d4f8e572a891b34d445
luis revilla <lrev4766@gmail.com>
2024-10-24 14:51:42 -0500

    5

--- install.sh
+++ install.sh
@@ -1,68 +1,46 @@
+#!/usr/bin/env bash
 
-echo "Installing Ubuntu updates"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
 
-echo "Upgrading pip and setuptools globally"
+apt-get install -y python3-pip
 python3 -m pip install --upgrade pip setuptools
 
-echo "Deleting ComfyUI"
 rm -rf /workspace/ComfyUI
-
-echo "Deleting venv"
 rm -rf /workspace/venv
 
-echo "Cloning ComfyUI repo to /workspace"
 cd /workspace
 git clone --depth=1 https://github.com/comfyanonymous/ComfyUI.git
 
-echo "Creating and activating venv"
 cd ComfyUI
 python3 -m venv /workspace/venv
+
 source /workspace/venv/bin/activate
+/workspace/venv/bin/python -m ensurepip --upgrade
+/workspace/venv/bin/python -m pip install --upgrade pip setuptools
 
-echo "Upgrading pip and setuptools in the venv"
 pip install --upgrade pip setuptools
-
-echo "Installing Torch"
 pip install --no-cache-dir torch==2.1.2+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-
-echo "Installing xformers"
 pip install --no-cache-dir xformers==0.0.23.post1+cu118 --index-url https://download.pytorch.org/whl/cu118
-
-echo "Installing ComfyUI requirements"
 pip install -r requirements.txt
 
-echo "Installing ComfyUI Manager"
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
 cd custom_nodes/ComfyUI-Manager
 pip install -r requirements.txt
 
-echo "Installing RunPod Serverless dependencies"
 pip install huggingface_hub runpod
 
-echo "Downloading SD 1.5 base model"
 cd /workspace/ComfyUI/models/checkpoints
 wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
-
-echo "Downloading Deliberate v2 model"
 wget -O deliberate_v2.safetensors https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors
-
-echo "Downloading SDXL base model"
 wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-
-echo "Downloading SDXL Refiner"
 wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
 
-echo "Downloading SD 1.5 VAE"
 cd /workspace/ComfyUI/models/vae
 wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
-
-echo "Downloading SDXL VAE"
 wget https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors
 
-echo "Downloading SD 1.5 ControlNet models"
 cd /workspace/ComfyUI/models/controlnet
 wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
 wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
@@ -72,13 +50,10 @@ wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p
 wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
 wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
 
-echo "Downloading SDXL ControlNet models"
 wget https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors
 
-echo "Downloading Upscalers"
 cd /workspace/ComfyUI/models/upscale_models
 wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
 wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
 
-echo "Creating log directory"
 mkdir -p /workspace/logs
